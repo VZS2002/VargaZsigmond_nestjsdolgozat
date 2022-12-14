1ab88090d088ff721870b59c8215ea25ad5245a0
Varga Zsigmond <vargazsigmond02@gmail.com>
2022-12-14 14:06:16 +0100

    felvetel + kereses

--- src/app.controller.ts
+++ src/app.controller.ts
@@ -1,3 +1,4 @@
+import { Query } from '@nestjs/common';
 import { Controller, Get, Render } from '@nestjs/common';
 import { AppService } from './app.service';
 import db from './db';
@@ -8,10 +9,42 @@ export class AppController {
 
   @Get()
   @Render('index')
-  async index() {
-    const [rows] = await db.execute('select * from macskak order by macskak.suly desc');
-    return {
-      macskak: rows,
-    };
+  
+  async listMacskak(@Query('szemszin') szemszin) {
+    
+    if (szemszin != null) {
+     
+      const [rows] = await db.execute(
+        'select * from macskak where macskak.szem_szin like ?',
+        [szemszin],
+      );
+
+
+      return {
+        macskak: rows,
+      };
+      
+    } else {
+     
+      const [rows] = await db.execute(
+        'select * from macskak order by macskak.suly desc',
+
+      );
+
+      return {
+        macskak: rows,
+      };
+   
+   
+    }
+
   }
+  
+  @Get('cats/new')
+  @Render('felvetel')
+  newCatForm() {
+    return {};
+  }
+  
+
 }
